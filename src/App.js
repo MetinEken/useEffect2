1012a4d28064b23ef7de91542d3c9d73bfe10184
MetinEken <metineken2021@gmail.com>
2022-07-27 22:26:38 +0200

    ilk

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import UseEffectFetch from "./UseEffectFetch";
+
+
+
+function App() {
+  return (
+    <div className="App">
+ 
+      <UseEffectFetch />
+   
+  
+    </div>
+  );
+}
+export default App;
